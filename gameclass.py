0d3fc9fcd30923ddf91ca617aef4c8431968ce23
Mr Blue <mblue@localhost.localdomain>
2020-02-17 14:00:59 -0500

    added discard and draw functions

--- gameclass.py
+++ gameclass.py
@@ -6,11 +6,21 @@ class Game:
         self.hand = hand
         self.players = players
         self.hands = [list() for player in range(players)]
+        self.discardpile = list()
 
     def deal(self):
-        for count in range(self.hand):
+        for _ in range(self.hand):
             for player in range(self.players):
                 self.hands[player].append(self.deck.pop())
+                
+    def discard(self, player, card):
+        hand = self.hands[player]
+        dropcard = hand.pop(card)
+        self.discardpile.append(dropcard)
+
+    def draw(self, pile, player):
+        self.hands[player].append(pile.pop())
+
 
 if __name__ == "__main__":
     deck = {
@@ -21,12 +31,21 @@ if __name__ == "__main__":
         (4,"diamonds"),
         (1,"spades"),
         (7,"hearts"),
-        (9,"hearts")}
+        (9,"hearts"),
+        (5,"clubs"),
+        (6,"clubs"),
+        (9,"clubs"),
+    }
     mygame = Game(deck, 2, 4)
 
     mygame.deal()
 
     print(mygame.hands)
-                
+    mygame.draw(mygame.deck, 0)
+    print(mygame.hands[0])
+    mygame.discard(0,1)
+    print("discard {}, hand {}".format(mygame.discardpile, mygame.hands[0]))
+    mygame.draw(mygame.discardpile, 1)
+    print("\nplayer1 hand {}".format(mygame.hands[1]))
         
         
